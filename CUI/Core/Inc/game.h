e8b985e6fa9241089c755d284badf347b7420620
Vitaliy Levchyk <144386741+VitaliyLevchyk@users.noreply.github.com>
2026-02-18 13:28:47 +0200

    Refactor header guards and include statements in game.h

--- CUI/Core/Inc/game.h
+++ CUI/Core/Inc/game.h
@@ -1,16 +1,18 @@
-#ifndef __GAME_H
-#define __GAME_H
+#ifndef INC_GAME_H_
+#define INC_GAME_H_
 
-#include "main.h"
+#include <stdint.h>
 
 // Розміри поля
 #define BOARD_ROWS 8
 #define BOARD_COLS 8
 
-// Прототипи функцій (вони мають точно збігатися з тими, що у game.c)
-void Game_Init(void);
-void Game_FillEmptyCells(void);
-void Game_ProcessStep(uint8_t column);
-void Game_SendBoardUART(void);
+// Глобальні змінні
+extern uint8_t board[BOARD_ROWS][BOARD_COLS];
+extern uint32_t score;
 
-#endif
+// Лише ці дві функції мають бути доступні для main.c
+void Game_Init(void);
+uint8_t Game_Swap(uint8_t r1, uint8_t c1, uint8_t r2, uint8_t c2);
+
+#endif /* INC_GAME_H_ */
